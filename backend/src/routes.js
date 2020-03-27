436d94627eedca32dc56b79793c6cf792bee1bc1
Yuri Duarte <yuriduarte@MacBook-Pro-de-Yuri.local>
2020-03-27 11:42:52 -0300

    Adiciona tese do recurso ONG com Jest

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -21,7 +21,7 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10),
+    whatsapp: Joi.string().required().min(10),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2)
   })
